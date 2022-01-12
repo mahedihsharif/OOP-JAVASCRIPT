b605768d2d82bb8dd7ed4d06b8d3331477f714cd
mahedi5061 <mahedi5061@gmail.com>
2022-01-12 10:21:07 +0000

    oop-javascript

---
+++ class.js
@@ -0,0 +1,9 @@
+class Person{
+    constructor(name, work, salaray){
+        this.name=name;
+        this.work=work;
+        this.salaray=salaray
+    }
+}
+const person1=new Person('Mahedi', 'student', 1000);
+console.log(person1)
