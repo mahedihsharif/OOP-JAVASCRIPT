c9f488874eefea64619024ac7a2113ddd4c3ee09
mahedi5061 <mahedi5061@gmail.com>
2022-01-13 04:12:22 +0000

    update functional oriented

--- class.js
+++ class.js
@@ -7,3 +7,12 @@ class Person{
 }
 const person1=new Person('Mahedi', 'student', 1000);
 console.log(person1)
+ 
+ //function oriented..
+ function Buisnessman(name, work, salaray){
+     this.name=name;
+     this.work=work;
+     this.salaray=salaray
+ }
+ var person2=new Buisnessman('sayem', 'webdeveloper', 50000);
+ console.log(person2.name)
